bbad319cd6589c7bb89931522146b7eb74eab82d
Pranav Dewangan <pranavdewangan3@gmail.com>
2025-02-10 09:19:05 +0530

    added more approach to solve the 5th problem

--- 5.leetcode-347-TopKFrequent.java
+++ 5.leetcode-347-TopKFrequent.java
@@ -7,7 +7,7 @@ class Solution {
         Solution solution = new Solution();
         int[] nums = {1, 1, 1, 2, 2, 3};
         int k = 2;
-        int[] result = solution.topKFrequent(nums, k);
+        int[] result = solution.topKFrequent2(nums, k);
         for (int num : result) {
             System.out.print(num + " ");
         }
@@ -33,6 +33,33 @@ class Solution {
             ans[i++] = maxHeap.poll().getKey();
             if(i==k)
                 break;
+        }
+        return ans;
+    }
+
+    public int[] topKFrequent2(int[] nums, int k) {
+        if(k==nums.length)
+            return nums;
+
+        Map<Integer,Integer> map=new HashMap<>();
+        for(int num:nums){
+            map.put(num,map.getOrDefault(num,0)+1);
+        }
+
+        PriorityQueue<Integer> heap = new PriorityQueue<>(
+            (a, b) -> map.get(a)-map.get(b)
+        );
+
+        for(int num:map.keySet()){
+            heap.add(num);
+            if(heap.size()>k){
+                heap.poll();
+            }
+        }
+
+        int[] ans = new int[k];
+        for(int i=0;i<k;i++){
+            ans[i]=heap.poll();
         }
         return ans;
     }
